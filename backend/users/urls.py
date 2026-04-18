4d3f5db6cffdd827276ed7f3dd77a056194229f9
Sia Chen Girl <chengirl222@gmail.com>
2026-04-18 17:31:03 +0800

    Complete forgot password(Django official)

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('api/register/', views.register),
@@ -7,4 +8,27 @@ urlpatterns = [
     path('api/logout/', views.user_logout),
     path('api/profile/', views.user_profile),
     path('api/change-password/', views.change_password),
+    path(
+    'api/password-reset/',
+    auth_views.PasswordResetView.as_view(),
+    name='password_reset'
+    ),
+
+    path(
+        'api/password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'
+    ),
+
+    path(
+        'api/reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
+
+    path(
+        'api/reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'
+    ),
 ]
